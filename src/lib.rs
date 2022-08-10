6f80328e549291bc5f5e8bf791905d93ee0b0ecf
NiiightmareXD <contact.aliiranpour@gmail.com>
2022-08-10 20:18:15 +0430

    Fix a bug and better document
    	modified:   src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,21 @@
 //!
 //! `simple-crypt` is a high level library to encrypt and decrypt data
 //!
+//! For security it uses aes-gcm-siv-256 and argon2
+//!
+//! # Examples
+//!
+//! Encrypting
+//! ```
+//! let encrypted_data = encrypt(b"example text", "example passowrd").expect("Failed to encrypt");
+//! ```
+//!
+//! Decrypting
+//! ```
+//! let data = decrypt(&encrypted_data, "example passowrd").expect("Failed to decrypt");
+//! ```
+//!
+
 use aes_gcm_siv::{
     aead::{generic_array::GenericArray, rand_core::RngCore, Aead, OsRng},
     Aes256GcmSiv, KeyInit, Nonce,
@@ -18,7 +33,18 @@ struct PrecryptorFile {
     salt: [u8; 32],
 }
 
-pub fn encrypt(data: &mut [u8], password: &str) -> Result<Vec<u8>> {
+/// Encrypts some data and return the result
+///
+/// # Examples
+///
+/// Encrypt
+/// ```
+/// let encrypted_data = encrypt(b"example text", "example passowrd").expect("Failed to encrypt");
+/// // and now you can write it to a file:
+/// // fs::write("test.enc", encrypted_data).expect("Failed to write to file");
+/// ```
+///
+pub fn encrypt(data: &[u8], password: &str) -> Result<Vec<u8>> {
     trace!("Generating salt");
     let mut salt = [0u8; 32];
     OsRng.fill_bytes(&mut salt);
@@ -57,10 +83,22 @@ pub fn encrypt(data: &mut [u8], password: &str) -> Result<Vec<u8>> {
     Ok(encoded)
 }
 
-pub fn decrypt(data: &mut [u8], password: &str) -> Result<Vec<u8>> {
+/// Decrypts some data and return the result
+///
+/// # Examples
+///
+/// ```
+/// let data = decrypt(&encrypted_data, "example passowrd").expect("Failed to decrypt");
+/// // and now you can print it to stdout:
+/// // println!("data: {}", String::from_utf8(data.clone()).expect("Data is not a utf8 string"));
+/// // or you can write it to a file:
+/// // fs::write("test.txt", data).expect("Failed to write to file");
+/// ```
+///
+pub fn decrypt(data: &[u8], password: &str) -> Result<Vec<u8>> {
     trace!("Decoding");
     let decoded: PrecryptorFile =
-        bincode::deserialize(&data[..]).with_context(|| "Failed to decode data")?;
+        bincode::deserialize(data).with_context(|| "Failed to decode data")?;
 
     let config = Config {
         hash_length: 32,
@@ -76,10 +114,23 @@ pub fn decrypt(data: &mut [u8], password: &str) -> Result<Vec<u8>> {
     let nonce = Nonce::from_slice(&decoded.nonce);
 
     info!("Decrypting");
-    let text = match cipher.decrypt(nonce, data.as_ref()) {
+    let text = match cipher.decrypt(nonce, decoded.data.as_ref()) {
         Ok(ciphertext) => ciphertext,
         Err(_) => return Err(anyhow!("Failed to encrypt data -> invalid password")),
     };
 
     Ok(text)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let encrypted_data =
+            encrypt(b"example text", "example passowrd").expect("Failed to encrypt");
+        let data = decrypt(&encrypted_data, "example passowrd").expect("Failed to decrypt");
+        assert_eq!(data, b"example text");
+    }
 }
