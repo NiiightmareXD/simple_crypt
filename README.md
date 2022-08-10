a848e48422ba8e94d35b0bf50dba168abc3871c7
NiiightmareXD <contact.aliiranpour@gmail.com>
2022-08-10 19:25:29 +0430

    Initial commit 	new file:   .gitignore 	new file:   Cargo.toml 	new file:   LICENSE 	new file:   README.md 	new file:   src/lib.rs

---
+++ README.md
@@ -0,0 +1,2 @@
+# simple-crypt
+A simple and high level library to encrypt and decrypt text, files and any data with it
---
+++ src/lib.rs
@@ -0,0 +1,85 @@
+//! # Simple Crypt
+//!
+//! `simple-crypt` is a high level library to encrypt and decrypt data
+//!
+use aes_gcm_siv::{
+    aead::{generic_array::GenericArray, rand_core::RngCore, Aead, OsRng},
+    Aes256GcmSiv, KeyInit, Nonce,
+};
+use anyhow::{anyhow, Context, Result};
+use argon2::Config;
+use log::{info, trace};
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct PrecryptorFile {
+    data: Vec<u8>,
+    nonce: [u8; 12],
+    salt: [u8; 32],
+}
+
+pub fn encrypt(data: Vec<u8>, password: &str) -> Result<Vec<u8>> {
+    trace!("Generating salt");
+    let mut salt = [0u8; 32];
+    OsRng.fill_bytes(&mut salt);
+    let config = Config {
+        hash_length: 32,
+        ..Default::default()
+    };
+
+    trace!("Generating key");
+    let password = argon2::hash_raw(password.as_bytes(), &salt, &config)
+        .with_context(|| "Failed to generate key from password")?;
+
+    let key = GenericArray::from_slice(&password);
+    let cipher = Aes256GcmSiv::new(key);
+
+    trace!("Generating nonce");
+    let mut nonce_rand = [0u8; 12];
+    OsRng.fill_bytes(&mut nonce_rand);
+    let nonce = Nonce::from_slice(&nonce_rand);
+
+    info!("Encrypting");
+    let ciphertext = match cipher.encrypt(nonce, data.as_ref()) {
+        Ok(ciphertext) => ciphertext,
+        Err(_) => return Err(anyhow!("Failed to encrypt data -> invalid password")),
+    };
+
+    let file = PrecryptorFile {
+        data: ciphertext,
+        nonce: nonce_rand,
+        salt,
+    };
+
+    trace!("Encoding");
+    let encoded: Vec<u8> = bincode::serialize(&file).with_context(|| "Failed to decode data")?;
+
+    Ok(encoded)
+}
+
+pub fn decrypt(data: Vec<u8>, password: &str) -> Result<Vec<u8>> {
+    trace!("Decoding");
+    let decoded: PrecryptorFile =
+        bincode::deserialize(&data[..]).with_context(|| "Failed to decode data")?;
+
+    let config = Config {
+        hash_length: 32,
+        ..Default::default()
+    };
+
+    trace!("Generating key");
+    let password = argon2::hash_raw(password.as_bytes(), &decoded.salt, &config)
+        .with_context(|| "Failed to generate key from password")?;
+
+    let key = GenericArray::from_slice(&password);
+    let cipher = Aes256GcmSiv::new(key);
+    let nonce = Nonce::from_slice(&decoded.nonce);
+
+    info!("Decrypting");
+    let text = match cipher.decrypt(nonce, data.as_ref()) {
+        Ok(ciphertext) => ciphertext,
+        Err(_) => return Err(anyhow!("Failed to encrypt data -> invalid password")),
+    };
+
+    Ok(text)
+}
